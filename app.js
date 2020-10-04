f62ef1ecbd19decdfa745d072211f7c220934304
cyrus8050 <ckmobile8050@gmail.com>
2020-10-04 10:13:40 +0800

    init

---
+++ app.js
@@ -0,0 +1,33 @@
+var STATIC_FILES = [
+    '/',
+    '/index.html',
+    '/offline.html',
+    '/src/js/app.js',
+    '/src/js/feed.js',
+    '/src/js/promise.js',
+    '/src/js/fetch.js',
+    '/src/js/material.min.js',
+    '/src/css/app.css',
+    '/src/css/feed.css',
+    '/src/images/main-image.jpg',
+    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+];
+
+function isInArray(string, array) {
+    var cachePath;
+    console.log('self', self.origin)
+    if (string.indexOf(self.origin) === 0) { // request targets domain where we serve the page from (i.e. NOT a CDN)
+        console.log('matched ', string);
+        cachePath = string.substring(self.origin.length); // take the part of the URL AFTER the domain (e.g. after localhost:8080)
+    } else {
+        cachePath = string; // store the full request (for CDNs)
+    }
+    console.log('cache path', cachePath)
+    return array.indexOf(cachePath) > -1;
+}
+
+isInArray('http://127.0.0.1:5500/index.html', STATIC_FILES)
+
+console.log(isInArray('http://127.0.0.1:5500/index.html', STATIC_FILES))
